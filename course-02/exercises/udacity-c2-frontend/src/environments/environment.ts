db84eed39003de8f729cc6ca194b6cbc232fc0b8
Quentin Van Overmeere <49268972+vquentin@users.noreply.github.com>
2020-05-28 23:29:42 +0200

    corrected bug in dependencies and in environment to host api

--- course-02/exercises/udacity-c2-frontend/src/environments/environment.ts
+++ course-02/exercises/udacity-c2-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://http://udagram-qvanover-dev-dev.us-east-1.elasticbeanstalk.com/api/v0'
+  apiHost: 'http://udagram-qvanover-dev-dev.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
